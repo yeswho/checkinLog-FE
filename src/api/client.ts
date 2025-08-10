3114f14edc20d1de9bf6b00c5598dfeeb474b8b9
Yeshu <yeshu6221@gmail.com>
2025-08-10 12:44:25 +0545

    add be

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://checkinlog-production.up.railway.app/api",
   headers: {
     "Content-Type": "application/json",
   },
