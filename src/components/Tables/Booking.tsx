dc1b6dccd621b772cf20468a95735eba7f7b64ca
Yeshu <yeshu6221@gmail.com>
2025-10-19 06:03:16 +0545

    load fix

--- src/components/Tables/Booking.tsx
+++ src/components/Tables/Booking.tsx
@@ -343,18 +343,6 @@ export default function BookingsTable() {
     );
   }, [selectedKeys, bookings.length, page, total, rowsPerPage, hasSearchFilter]);
 
-  if (isLoading) return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm z-50">
-      <Spinner
-        classNames={{
-          base: "scale-150",
-          label: "text-foreground mt-4",
-        }}
-        color="primary"
-      />
-    </div>
-  );
-
   if (isError) return <div>Error fetching bookings</div>;
 
   return (
@@ -388,7 +376,20 @@ export default function BookingsTable() {
             </TableColumn>
           )}
         </TableHeader>
-        <TableBody emptyContent={null} items={bookings}>
+        <TableBody
+          emptyContent={isLoading ? (
+            <Spinner
+              classNames={{
+                base: "scale-150",
+                label: "text-foreground mt-4",
+              }}
+              color="primary"
+            />
+          ) : (
+            "No bookings found"
+          )}
+          items={bookings}
+        >
           {(item) => (
             <TableRow key={item.id}>
               {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
